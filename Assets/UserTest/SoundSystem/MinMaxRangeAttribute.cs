ee0dc415d270fcf8ce4321451cc6ff8d1db0027f
不跳的马小跳 <45055864+Crazyfengyi@users.noreply.github.com>
2022-07-03 00:44:28 +0800

    更新
    声音SO配置写法记录

---
+++ Assets/UserTest/SoundSystem/MinMaxRangeAttribute.cs
@@ -0,0 +1,21 @@
+ /** 
+ *Copyright(C) 2020 by DefaultCompany 
+ *All rights reserved. 
+ *Author:       DESKTOP-AJS8G4U 
+ *UnityVersion：2022.1.0f1c1 
+ *创建时间:         2022-07-02 
+*/  
+using UnityEngine;  
+using System.Collections;
+using System;
+
+public class MinMaxRangeAttribute : Attribute
+{
+	public MinMaxRangeAttribute(float min, float max)
+	{
+		Min = min;
+		Max = max;
+	}
+	public float Min { get; private set; }
+	public float Max { get; private set; }
+}

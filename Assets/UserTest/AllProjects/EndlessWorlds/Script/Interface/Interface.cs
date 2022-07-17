d31269b35a92bd643042dc5b9543bcf5059e23eb
不跳的马小跳 <45055864+Crazyfengyi@users.noreply.github.com>
2022-07-17 22:04:06 +0800

    更新
    更新

---
+++ Assets/UserTest/AllProjects/EndlessWorlds/Script/Interface/Interface.cs
@@ -0,0 +1,36 @@
+/** 
+ *Copyright(C) 2020 by DefaultCompany 
+ *All rights reserved. 
+ *Author:       DESKTOP-AJS8G4U 
+ *UnityVersion：2022.1.0f1c1 
+ *创建时间:         2022-05-21 
+*/
+using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 交互接口
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// 进入范围
+    /// </summary>
+    public void EnterRang()
+    {
+
+    }
+    /// <summary>
+    /// 退出范围
+    /// </summary>
+    public void ExitRang()
+    {
+
+    }
+    /// <summary>
+    /// 交互 
+    /// </summary>
+    public void InterAction()
+    {
+
+    }
+}
---
+++ Assets/UserTest/AllProjects/EndlessWorlds/Script/Tools/FollowTarget.cs
@@ -0,0 +1,28 @@
+/** 
+ *Copyright(C) 2020 by DefaultCompany 
+ *All rights reserved. 
+ *Author:       DESKTOP-AJS8G4U 
+ *UnityVersion：2021.2.1f1c1 
+ *创建时间:         2022-02-13 
+*/
+using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour
+{
+    #region 变量
+    public Transform target;
+    private Vector3 offset;
+    #endregion
+
+    #region 生命周期
+    private void Awake()
+    {
+        offset = target.position - transform.position;
+    }
+    private void LateUpdate()
+    {
+        transform.position = target.position - offset;
+    }
+    #endregion
+}

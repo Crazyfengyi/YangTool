7c78b1b555d428e0d0704092cf81d7d940e9f228
不跳的马小跳 <enunang@foxmail.com>
2025-09-16 13:38:24 +0800

    更新

--- Assets/YangTools/Scripts/Core/LanguageManager/LanguageManager.cs
+++ Assets/YangTools/Scripts/Core/LanguageManager/LanguageManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using cfg.language;
 using UnityEngine;
 using YangTools.Scripts.Core;
@@ -7,19 +9,42 @@ public class LanguageManager : MonoSingleton<LanguageManager>
 {
     public cfg.language.LanguageType languageType;
     
+    private Dictionary<string, PropertyInfo> languageDic = new ();
+    private TBLanguage tableData;
+
+    public void Start()
+    {
+        tableData = GameTableManager.Instance.Tables.TBLanguage;
+        Type type = typeof(Language);
+        //获取所有实例属性
+        PropertyInfo[] allFields = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        foreach (PropertyInfo field in allFields)
+        {
+            languageDic.Add(field.Name, field);
+        }
+        
+        Debug.LogError($"测试:{GetLanguage("textStrId_1")}");
+    }
+
     public string GetLanguage(string key)
     {
-        //考虑反射获取字段或其它自动化高的配表方案
-        //Language temp = GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key);
+        // switch (languageType)
+        // {
+        //     case cfg.language.LanguageType.Chinese:
+        //         return GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key).Chinese;
+        //     case cfg.language.LanguageType.English:
+        //         return GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key).English;
+        //     default:
+        //         return GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key).Chinese;
+        // }
         
-        switch (languageType)
+        //考虑反射获取字段或其它自动化高的配表方案
+        string typeStr = languageType.ToString();
+        if (languageDic.TryGetValue(typeStr,out PropertyInfo fieldInfo))
         {
-            case cfg.language.LanguageType.Chinese:
-                return GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key).Chinese;
-            case cfg.language.LanguageType.English:
-                return GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key).English;
-            default:
-                return GameTableManager.Instance.Tables.TBLanguage.GetOrDefault(key).Chinese;
+            object temp = fieldInfo.GetValue(tableData.GetOrDefault(key));
+            return temp.ToString();
         }
+        return $"{key}:未找到多语言值";
     } 
 }
